7a17f5a439af6b79e40953eab5369186c0838237
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-07-26 14:45:45 +0530

    Find Maximum Number of Float value

--- Day13GenericProblem/MaximumNumber.cs
+++ Day13GenericProblem/MaximumNumber.cs
@@ -26,7 +26,27 @@ namespace Day13GenericProblem
 
 
         }
+        public static float MaximumFloatValue(float FirstValue, float SecondValue, float ThirdValue)
+        {
+            if (FirstValue.CompareTo(SecondValue) > 0 && FirstValue.CompareTo(ThirdValue) > 0 || FirstValue.CompareTo(SecondValue) >= 0 && FirstValue.CompareTo(ThirdValue) > 0 || FirstValue.CompareTo(SecondValue) > 0 && FirstValue.CompareTo(ThirdValue) >= 0)
+            {
+                return FirstValue;
+            }
+            if (SecondValue.CompareTo(FirstValue) > 0 && SecondValue.CompareTo(ThirdValue) > 0 || SecondValue.CompareTo(FirstValue) >= 0 && SecondValue.CompareTo(ThirdValue) > 0 || SecondValue.CompareTo(FirstValue) > 0 && SecondValue.CompareTo(ThirdValue) >= 0)
+            {
+                return SecondValue;
+            }
+            if (ThirdValue.CompareTo(FirstValue) > 0 && ThirdValue.CompareTo(SecondValue) > 0 || ThirdValue.CompareTo(FirstValue) >= 0 && ThirdValue.CompareTo(SecondValue) > 0 || ThirdValue.CompareTo(FirstValue) > 0 && ThirdValue.CompareTo(SecondValue) >= 0)
+            {
+                return ThirdValue;
+            }
+            return FirstValue;
+        }
+
 
 
     }
+
+
 }
+
--- Day13GenericProblem/Program.cs
+++ Day13GenericProblem/Program.cs
@@ -8,11 +8,13 @@ namespace  Day13GenericProblem
 {
     public class program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) 
         {
             Console.WriteLine(" Find The Maximum Number!");
             int output = MaximumNumber.AMaximumNumber(11, 22, 33);
             Console.WriteLine(output);
+            float output1 = MaximumNumber.MaximumFloatValue(5.4f, 52.4f, 11.5f);
+            Console.WriteLine(output1);
 
         }
 
