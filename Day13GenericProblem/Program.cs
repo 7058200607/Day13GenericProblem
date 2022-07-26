e4be0df9eb2398c134acdfa612e51dd2deb2ee12
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-07-26 14:38:10 +0530

    Find maximum Number

---
+++ Day13GenericProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace  Day13GenericProblem
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Find The Maximum Number!");
+            int output = MaximumNumber.AMaximumNumber(11, 22, 33);
+            Console.WriteLine(output);
+
+        }
+
+
+    }
+}
